836ba422d3e4a516e111cb017c47389fa6f8d85a
Nata-v <nataliavolkova.by@gmail.com>
2023-11-11 17:11:30 +0300

    task2task3refresh

--- Modul3Task2Task3/src/main/resources/schema.sql
+++ Modul3Task2Task3/src/main/resources/schema.sql
@@ -10,9 +10,17 @@ CREATE TABLE IF NOT EXISTS receivers (
 CREATE TABLE IF NOT EXISTS expenses (
   id INT PRIMARY KEY AUTO_INCREMENT,
  payment_date DATE NOT NULL,
-  receiver_id INT,
+  receivers_id INT,
   amount DECIMAL(10,2),
-  FOREIGN KEY (receiver_id) REFERENCES receivers (id)
+  FOREIGN KEY (receivers_id) REFERENCES receivers (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS deleted_expenses (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+ payment_date DATE NOT NULL,
+  receiver_id INT,
+  total_amount DECIMAL(10,2)
 );
 
 INSERT INTO receivers(name) VALUES ('MTC');
@@ -22,8 +30,22 @@ INSERT INTO receivers (name)VALUES ('Telekom');
 
 
 
-INSERT INTO expenses (payment_date,receiver_id, amount)VALUES ('2022-05-10', 1, 20000);
-INSERT INTO expenses (payment_date,receiver_id, amount) VALUES ('2022-06-23', 2,48000);
-INSERT INTO expenses (payment_date,receiver_id, amount) VALUES ('2022-05-11', 4,30000);
-INSERT INTO expenses (payment_date,receiver_id, amount) VALUES ('2022-03-12', 1, 70000);
-INSERT INTO expenses (payment_date,receiver_id, amount)VALUES ('2022-04-12',3,90000);
+INSERT INTO expenses (payment_date,receivers_id, amount)VALUES ('2022-05-10', 1, 20000);
+INSERT INTO expenses (payment_date,receivers_id, amount) VALUES ('2022-06-23', 2,48000);
+INSERT INTO expenses (payment_date,receivers_id, amount) VALUES ('2022-05-11', 4,30000);
+INSERT INTO expenses (payment_date,receivers_id, amount) VALUES ('2022-03-12', 1, 70000);
+INSERT INTO expenses (payment_date,receivers_id, amount)VALUES ('2022-04-12',3,90000);
+
+CREATE TRIGGER expenses_trigger
+BEFORE DELETE ON expenses
+FOR EACH ROW
+BEGIN
+    DECLARE total_amount DECIMAL(10, 2);
+    DECLARE receiver_name VARCHAR(256);
+
+    SELECT SUM(amount) INTO total_amount FROM expenses WHERE receivers_id = OLD.receivers_id;
+    SELECT name INTO receiver_name FROM receivers WHERE id = OLD.receivers_id;
+
+
+    INSERT INTO deleted_expenses (payment_date, receiver_name, total_amount) VALUES (OLD.payment_date, receiver_name, total_amount);
+END;
