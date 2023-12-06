7a05e213c79f892d00c80008f1fce3c77acd3b0c
Nata-v <nataliavolkova.by@gmail.com>
2023-12-06 12:28:09 +0300

    changeTests

--- Modul3Task2Task3/src/test/java/by/nata/data/dao/HibernateRunTest.java
+++ Modul3Task2Task3/src/test/java/by/nata/data/dao/HibernateRunTest.java
@@ -17,187 +17,194 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class HibernateRunTest {
-        private static SessionFactory sessionFactory;
-        private static Dao dao;
-        private Session session = null;
-        private Transaction transaction = null;
-        @Before
-        public void setUp() throws Exception {
+    private static SessionFactory sessionFactory;
+    private static Dao dao;
+    private Session session = null;
+    private Transaction transaction = null;
 
-                Connection conn = JdbcUtilTest.getConnection();
-                conn.createStatement().executeUpdate("DELETE FROM expenses;");
-                conn.createStatement().executeUpdate("DELETE FROM receivers;");
+    @Before
+    public void setUp() throws Exception {
+        sessionFactory = HibernateUtilTest.getSessionFactory();
+        sessionFactory.openSession();
+
+        Connection conn = JdbcUtilTest.getConnection();
+        conn.createStatement().executeUpdate("DELETE FROM expenses;");
+        conn.createStatement().executeUpdate("DELETE FROM receivers;");
 
 
-            sessionFactory = HibernateUtilTest.getSessionFactory();
-            sessionFactory.openSession();
 
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        if (sessionFactory == null) {
+            sessionFactory.close();
         }
-
-        @After
-        public void tearDown() throws Exception {
-
-            if (sessionFactory == null) {
-                sessionFactory.close();
-            }
-                dao = null;
-                Connection conn = JdbcUtilTest.getConnection();
-                conn.createStatement().executeUpdate("DELETE FROM expenses;");
-                conn.createStatement().executeUpdate("DELETE FROM receivers;");
+        dao = null;
+        Connection conn = JdbcUtilTest.getConnection();
+        conn.createStatement().executeUpdate("DELETE FROM expenses;");
+        conn.createStatement().executeUpdate("DELETE FROM receivers;");
 
 
-        }
+    }
 
 
-        @Test
-        public void save() throws SQLException, ClassNotFoundException {
+    @Test
+    public void save() throws SQLException, ClassNotFoundException {
 
 
-                session = sessionFactory.openSession();
-                transaction = session.beginTransaction();
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
 
-                Receivers receiver = new Receivers(null, "SAT-TV");
-
-
-                session.save(receiver);
-                Expenses expenses = new Expenses(null, LocalDate.now(), new Receivers(null, "DANA-MOLL"), 3000.00);
-                session.save(expenses);
-
-                transaction.commit();
-
-                assertNotNull(receiver);
-                Connection conn = JdbcUtilTest.getConnection();
-                ResultSet rs = conn.createStatement().executeQuery("select count(*) from receivers where NAME='SAT-TV'");
-                rs.next();
-
-                int actualCount = rs.getInt(1);
-
-                assertEquals(1, actualCount);
+        Receivers receiver = new Receivers(null, "SAT-TV");
 
 
+        session.save(receiver);
+        Expenses expenses = new Expenses(null, LocalDate.now(), new Receivers(null, "DANA-MOLL"), 3000.00);
+        session.save(expenses);
 
-        }
+        transaction.commit();
 
-        @Test
-        public void findById() throws SQLException, ClassNotFoundException {
+        assertNotNull(receiver);
+        Connection conn = JdbcUtilTest.getConnection();
+        ResultSet rs = conn.createStatement().executeQuery("select count(*) from receivers where NAME='SAT-TV'");
+        rs.next();
 
-                session = sessionFactory.openSession();
-                transaction = session.beginTransaction();
+        int actualCount = rs.getInt(1);
 
-                Receivers receiver = new Receivers(null, "MTC");
-                session.save(receiver);
-                transaction.commit();
+        assertEquals(1, actualCount);
 
-                assertNotNull(receiver);
-                Connection conn = JdbcUtilTest.getConnection();
-                ResultSet rs = conn.createStatement().executeQuery("select count(*) from receivers where NAME='MTC'");
-                rs.next();
+        conn.close();
 
-                int actualCount = rs.getInt(1);
+    }
 
-                assertEquals(1, actualCount);
+    @Test
+    public void findById() throws SQLException, ClassNotFoundException {
 
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
 
-        }
+        Receivers receiver = new Receivers(null, "MTC");
+        session.save(receiver);
+        transaction.commit();
 
+        assertNotNull(receiver);
+        Connection conn = JdbcUtilTest.getConnection();
+        ResultSet rs = conn.createStatement().executeQuery("select count(*) from receivers where NAME='MTC'");
+        rs.next();
 
-        @Test
-        public void delete() throws SQLException, ClassNotFoundException {
+        int actualCount = rs.getInt(1);
 
-                session = sessionFactory.openSession();
-                transaction = session.beginTransaction();
+        assertEquals(1, actualCount);
+        conn.close();
 
-                Expenses expenses = new Expenses(null, LocalDate.now(), new Receivers(null, "DANA-MOLL"), 3000.00);
-                session.save(expenses);
-                transaction.commit();
-
-                Connection conn = JdbcUtilTest.getConnection();
-                ResultSet rs = conn.createStatement()
-                        .executeQuery("select count(*) from expenses where amount=3000.00");
-                rs.next();
-
-                int actualCount = rs.getInt(1);
-
-                assertEquals(1, actualCount);
+    }
 
 
-                assertEquals(null, session.get(Expenses.class, 5L));
+    @Test
+    public void delete() throws SQLException, ClassNotFoundException {
 
-        }
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
 
-        @Test
-        public void get() throws SQLException, ClassNotFoundException {
-                session = sessionFactory.openSession();
-                transaction = session.beginTransaction();
+        Expenses expenses = new Expenses(null, LocalDate.now(), new Receivers(null, "DANA-MOLL"), 3000.00);
+        session.save(expenses);
+        transaction.commit();
 
-                Receivers receiver = new Receivers(null, "Life");
-                session.save(receiver);
-                transaction.commit();
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
+        Expenses expensesDelete = session.get(Expenses.class, expenses.getId());
+        if (expensesDelete != null)
+            session.delete(expensesDelete);
+          transaction.commit();
 
-                assertNotNull(receiver);
-                Connection conn = JdbcUtilTest.getConnection();
-                ResultSet rs = conn.createStatement().executeQuery("select count(*) from receivers where NAME='Life'");
-                rs.next();
+        Connection conn = JdbcUtilTest.getConnection();
 
-                int actualCount = rs.getInt(1);
+        ResultSet rs = conn.createStatement().executeQuery("select count(*) from expenses where amount=3000.00");
+        rs.next();
+        int actualCount = rs.getInt(1);
+        assertEquals(0,actualCount);
+        conn.close();
 
-                assertEquals(1, actualCount);
+    }
 
+    @Test
+    public void get() throws SQLException, ClassNotFoundException {
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
 
-        }
+        Receivers receiver = new Receivers(null, "Life");
+        session.save(receiver);
+        transaction.commit();
 
-        @Test
-        public void load() throws SQLException, ClassNotFoundException {
-                session = sessionFactory.openSession();
-                transaction = session.beginTransaction();
+        assertNotNull(receiver);
+        Connection conn = JdbcUtilTest.getConnection();
+        ResultSet rs = conn.createStatement().executeQuery("select count(*) from receivers where NAME='Life'");
+        rs.next();
 
-                Receivers receiver = new Receivers(null, "A1");
-                session.save(receiver);
-                transaction.commit();
+        int actualCount = rs.getInt(1);
 
-                assertNotNull(receiver);
+        assertEquals(1, actualCount);
+        conn.close();
 
-                Connection conn = JdbcUtilTest.getConnection();
-                ResultSet rs = conn.createStatement().executeQuery("select count(*) from receivers where NAME='A1'");
-                rs.next();
+    }
 
-                int actualCount = rs.getInt(1);
+    @Test
+    public void load() throws SQLException, ClassNotFoundException {
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
 
-                assertEquals(1, actualCount);
+        Receivers receiver = new Receivers(null, "A1");
+        session.save(receiver);
+        transaction.commit();
 
+        assertNotNull(receiver);
 
-        }
+        Connection conn = JdbcUtilTest.getConnection();
+        ResultSet rs = conn.createStatement().executeQuery("select count(*) from receivers where NAME='A1'");
+        rs.next();
 
+        int actualCount = rs.getInt(1);
 
-        @Test
-        public void deleteExpense() throws SQLException, ClassNotFoundException {
+        assertEquals(1, actualCount);
 
-                session = sessionFactory.openSession();
-                transaction = session.beginTransaction();
-
-                Expenses expense = new Expenses(null, LocalDate.now(), new Receivers(null, "OMO"), 5000.00);
-                session.save(expense);
-                session.flush();
-
-                session = sessionFactory.openSession();
-
-                Expenses expenseDelete = session.find(Expenses.class, expense.getId());
-                transaction.commit();
-                Connection conn = JdbcUtilTest.getConnection();
-                ResultSet rs = conn.createStatement()
-                        .executeQuery("select count(*) from expenses where amount=5000.00");
-                rs.next();
-
-                int actualCount = rs.getInt(1);
-
-                assertEquals(1, actualCount);
-
+        conn.close();
+    }
 
 
-        }
+    @Test
+    public void deleteExpense() throws SQLException, ClassNotFoundException {
+
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
+
+        Expenses expense = new Expenses(null, LocalDate.now(), new Receivers(null, "OMO"), 5000.00);
+        session.save(expense);
+        transaction.commit();
+
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
+
+        Expenses expenseDelete = session.find(Expenses.class, expense.getId());
+        session.delete(expenseDelete);
+        transaction.commit();
+
+        Connection conn = JdbcUtilTest.getConnection();
+        ResultSet rs = conn.createStatement()
+                .executeQuery("select count(*) from expenses where amount=5000.00");
+        rs.next();
+
+        int actualCount = rs.getInt(1);
+
+        assertEquals(0, actualCount);
+
+        conn.close();
+
+    }
 
 }
